500ccba8f026604d13715e39fb2f39d7a1a36268
rimasultana <rimasultana20225@gmail.com>
2024-08-27 11:42:08 +0600

    review section

--- src/components/Caragory.jsx
+++ src/components/Caragory.jsx
@@ -1,8 +1,48 @@
-
 export default function Caragory() {
   return (
     <>
-    <h1 className="bg-red-500">This is category</h1>
+      <section className="bg-black w-full">
+        <div className="w-11/12 mx-auto py-5 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 items-center gap-5">
+          <div className="w-[300px] bg-[#1e272e] flex items-center p-5">
+            <div className="text-3xl text-lime-400">
+              <i className="fa-solid fa-car-side"></i>
+            </div>
+            <div className="flex items-center flex-col ">
+              <h3 className="text-white font-semibold text-lg">Free Shipping</h3>
+              <p className="text-gray-400 ">Above $5 Only</p>
+            </div>
+          </div>
+          <div className="w-[300px] bg-[#1e272e] flex items-center p-5">
+            <div className="text-3xl text-lime-400">
+              <i className="fa-solid fa-car-side"></i>
+            </div>
+            <div className="flex items-center flex-col ">
+              <h3 className="text-white font-semibold text-lg">Free Shipping</h3>
+              <p className="text-gray-400 ">Above $5 Only</p>
+            </div>
+          </div>
+          <div className="w-[300px] bg-[#1e272e] flex items-center p-5">
+            <div className="text-3xl text-lime-400">
+              <i className="fa-solid fa-car-side"></i>
+            </div>
+            <div className="flex items-center flex-col ">
+              <h3 className="text-white font-semibold text-lg">Free Shipping</h3>
+              <p className="text-gray-400 ">Above $5 Only</p>
+            </div>
+          </div>
+          <div className="w-[300px] bg-[#1e272e] flex items-center p-5">
+            <div className="text-3xl text-lime-400">
+              <i className="fa-solid fa-car-side"></i>
+            </div>
+            <div className="flex items-center flex-col ">
+              <h3 className="text-white font-semibold text-lg">Free Shipping</h3>
+              <p className="text-gray-400 ">Above $5 Only</p>
+            </div>
+          </div>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -120,6 +120,5 @@ export default function Header() {
         </div>
       </section>
     </header>
-
   );
 }
